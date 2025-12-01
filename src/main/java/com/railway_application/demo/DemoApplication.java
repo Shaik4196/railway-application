1048c824f4606b5ff3ff5d8c7406a6e272829b67
masthan.shaik3949@gmail.com <masthan.shaik2026@gmail.com>
2025-12-01 17:04:55 +0530

    Added Endpoint

--- src/main/java/com/railway_application/demo/DemoApplication.java
+++ src/main/java/com/railway_application/demo/DemoApplication.java
@@ -2,9 +2,17 @@ package com.railway_application.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+    @GetMapping(path = "/get_user")
+    public String getUser(){
+        return "Hello Shaik Welcome To Spring Boot Application";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
